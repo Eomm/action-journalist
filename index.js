9be704075c1edce03685a1c393abd461c8cad0a6
Manuel Spigolon <behemoth89@gmail.com>
2019-11-11 23:36:19 +0100

    defining action

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+'use strict'
 
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const command = core.getInput('command');
+    // const filterKeywords = core.getInput('keywords'); // TODO
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
+    const githubToken = core.getInput('github-token');
+
+
+
+    let myOutput = '';
+    let myError = '';
+
+    const options = {};
+    options.listeners = {
+      stdline: (data) => {
+        myOutput += data;
+      },
+      errline: (data) => {
+        myError += data;
+      }
+    };
+
+    core.debug(`Running command ${command}`)
+    await exec.exec(command, options);
+    core.debug('Command executed')
+
+    const octokit = new github.GitHub(githubToken);
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number,
+      body
+    })
+    Parameters
+
+    wait(parseInt(command));
     core.debug((new Date()).toTimeString())
 
     core.setOutput('time', new Date().toTimeString());
-  } 
+  }
   catch (error) {
     core.setFailed(error.message);
   }
