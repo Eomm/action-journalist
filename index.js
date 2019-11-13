94f9735489baa7a688d3068db3f044e2429f93a9
Manuel Spigolon <behemoth89@gmail.com>
2019-11-13 10:01:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
 'use strict'
 
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
 
 // most @actions toolkit packages have async methods
-async function run() {
+async function run () {
   try {
-    const command = core.getInput('command');
-    // const filterKeywords = core.getInput('keywords'); // TODO
+    const command = core.getInput('command')
+    const filterKeywords = ['benchmark']// TODO core.getInput('keywords')
 
-    const githubToken = core.getInput('github-token');
+    const githubToken = core.getInput('github-token')
 
+    let myOutput = ''
+    let myError = ''
 
-
-    let myOutput = '';
-    let myError = '';
-
-    const options = {};
-    options.listeners = {
-      stdline: (data) => {
-        myOutput += data;
-      },
-      errline: (data) => {
-        myError += data;
+    const options = {
+      listeners: {
+        stdline: data => { myOutput += data },
+        errline: data => { myError += data }
       }
-    };
+    }
+
+    // https://developer.github.com/v3/activity/events/types/#pullrequestreviewcommentevent
+    // pull_request_review_comment
+    const event = require(process.env.GITHUB_EVENT_PATH)
+
+    const { comment, pull_request: pullRequest } = event
+    if (!filterKeywords.some(_ => comment.body.includes(_))) {
+      core.warn(`Ignoring comment ${comment.body}`)
+      return
+    }
 
     core.debug(`Running command ${command}`)
-    await exec.exec(command, options);
+    await exec.exec(command, options)
     core.debug('Command executed')
 
-    const octokit = new github.GitHub(githubToken);
+    core.debug('Creating comment in issue XXX')
+    const octokit = new github.GitHub(githubToken)
     await octokit.issues.createComment({
-      owner,
-      repo,
-      issue_number,
-      body
+      owner: process.env.GITHUB_ACTOR,
+      repo: process.env.GITHUB_REPOSITORY,
+      issue_number: pullRequest.number,
+      body: `StdOut:\n${myOutput}\n\n\nStdErr:\n${myError}`
     })
-    Parameters
-
-    wait(parseInt(command));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  }
-  catch (error) {
-    core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
