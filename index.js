81f85796b98c532fd53c41a183aa9f16dc7b9c82
Manuel Spigolon <behemoth89@gmail.com>
2019-11-13 14:50:20 +0100

    test implementation (#1)

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+'use strict'
 
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
 
 // most @actions toolkit packages have async methods
-async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+async function run () {
+  try {
+    const command = core.getInput('command')
+    const filterKeywords = ['benchmark']// TODO core.getInput('keywords')
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const githubToken = core.getInput('github-token')
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+    let myOutput = ''
+    let myError = ''
+
+    const options = {
+      listeners: {
+        stdline: data => { myOutput += data },
+        errline: data => { myError += data }
+      }
+    }
+
+    // https://developer.github.com/v3/activity/events/types/#pullrequestreviewcommentevent
+    // pull_request_review_comment
+    const event = require(process.env.GITHUB_EVENT_PATH)
+
+    const { comment, pull_request: pullRequest } = event
+    if (!filterKeywords.some(_ => comment.body.includes(_))) {
+      core.warn(`Ignoring comment ${comment.body}`)
+      return
+    }
+
+    core.debug(`Running command ${command}`)
+    await exec.exec(command, options)
+    core.debug('Command executed')
+
+    core.debug('Creating comment in issue XXX')
+    const octokit = new github.GitHub(githubToken)
+    await octokit.issues.createComment({
+      owner: process.env.GITHUB_ACTOR,
+      repo: process.env.GITHUB_REPOSITORY,
+      issue_number: pullRequest.number,
+      body: `StdOut:\n${myOutput}\n\n\nStdErr:\n${myError}`
+    })
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
